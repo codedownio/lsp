8fb750084d0cc8b4b0a8ee345ce5095031143b52
Tom McLaughlin <tom@codedown.io>
2022-11-03 03:10:59 -0700

    Try old withCreateProcess

--- lsp-test/src/Language/LSP/Test/Server.hs
+++ lsp-test/src/Language/LSP/Test/Server.hs
@@ -5,9 +5,10 @@ import Control.Monad.IO.Class
 import Control.Monad.IO.Unlift
 import Control.Monad.Logger
 import qualified Data.Text as T
+import Language.LSP.Test.Compat
 import System.IO
+import System.Process hiding (withCreateProcess)
 import UnliftIO.Async
-import UnliftIO.Process
 
 
 withServer :: (
@@ -19,11 +20,12 @@ withServer serverExe logStdErr modifyCreateProcess f = do
   let cmd:args = words serverExe
   let createProc = (proc cmd args) { std_in = CreatePipe, std_out = CreatePipe, std_err = CreatePipe }
 
-  withCreateProcess (modifyCreateProcess createProc) $ \(Just serverIn) (Just serverOut) (Just serverErr) serverProc -> do
-    -- Need to continuously consume stderr else it gets blocked
-    -- Can't pass NoStream either to std_err
-    liftIO $ hSetBuffering serverErr NoBuffering
-    liftIO $ hSetBinaryMode serverErr True
-    let errSinkThread = forever $ liftIO (hGetLine serverErr) >>= when logStdErr . logDebugN . T.pack
-    withAsync errSinkThread $ \_ -> do
-      f serverIn serverOut serverProc
+  withRunInIO $ \runInIO ->
+    withCreateProcess (modifyCreateProcess createProc) $ \(Just serverIn) (Just serverOut) (Just serverErr) serverProc -> do
+      -- Need to continuously consume stderr else it gets blocked
+      -- Can't pass NoStream either to std_err
+      liftIO $ hSetBuffering serverErr NoBuffering
+      liftIO $ hSetBinaryMode serverErr True
+      let errSinkThread = forever $ liftIO (hGetLine serverErr) >>= when logStdErr . logDebugN . T.pack
+      runInIO $ withAsync errSinkThread $ \_ ->
+        f serverIn serverOut serverProc
