844fa8fbde9fd57dd98b1ae9c5726e19fe08539c
Tom McLaughlin <tom@codedown.io>
2022-11-03 12:03:08 -0700

    Try forkIOWithUnmask

--- lsp-test/src/Language/LSP/Test/Parsing.hs
+++ lsp-test/src/Language/LSP/Test/Parsing.hs
@@ -90,7 +90,7 @@ satisfyMaybeM pred = do
     else Just <$> do
       chan <- asks messageChan
       timeout <- asks (messageTimeout . config)
-      liftIO $ forkIO $ do
+      liftIO $ forkIOWithUnmask $ \unmask -> unmask $ do
         threadDelay (timeout * 1000000)
         writeChan chan (TimeoutMessage timeoutId)
 
